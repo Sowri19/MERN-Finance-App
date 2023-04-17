9ade68160441c5fc950f72a1ba6b82345501fcf0
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2023-04-17 10:31:52 -0400

    setting up eslint, and google fonts

--- Finance App/client/src/App.tsx
+++ Finance App/client/src/App.tsx
@@ -4,13 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="app">
-     
-    </div>
-  )
+  return (<div className="app"></div>)
 }
 
 export default App
